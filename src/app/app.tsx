3f73e1a61e92328505162d780cbeb5c242de8189
Sapronov Konstantin <ks.costas@gmail.com>
2023-09-02 20:17:45 +0300

    feat: add jsx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,54 @@
-function App() {
-  return (
-    <div className="container mx-auto mt-5">
-      <h1 className="text-center text-5xl font-extrabold dark:text-white">Hello React</h1>
-    </div>
-  );
-}
+import { Fragment, ReactElement } from 'react';
+
+/**
+ * JSX — это расширение синтаксиса JavaScript,
+ * которое позволяет писать HTML-разметку внутри файла JavaScript.
+ * Файлы JSX имеют расширение tsx и jsx.
+ */
+
+const App = (): ReactElement => {
+  /** Простой элемент с одним тегом H1 */
+  const someJSX = <h1>Hello React</h1>;
+
+  /** Вложенные элементы */
+  // const someJSX = (
+  //   <div>
+  //     <h1>Hello React</h1>
+  //   </div>
+  // );
+
+  /** Фрагмент */
+  // const someJSX = (
+  //   <Fragment>
+  //     <h1>Hello React</h1>
+  //     <div>This is text</div>
+  //   </Fragment>
+  // );
+
+  /** Фрагмент */
+  // const someJSX = (
+  //   <>
+  //     <h1>Hello React</h1>
+  //     <div>This is text</div>
+  //   </>
+  // );
+
+  return someJSX;
+
+  /** Выражения */
+  // const expressions1 = <p>{1 + 2}</p>;
+  // const expressions2 = <p>{Math.random() * 100}</p>;
+  // const expressions3 = <p>{expressions1}</p>;
+
+  // return (
+  //   <>
+  //     <h1>Hello React</h1>
+  //     <div>
+  //       {expressions2}
+  //       {expressions3}
+  //     </div>
+  //   </>
+  // );
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './main.css';
+
 import App from '@/app/app.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
