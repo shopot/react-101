e4113181241be3a46e46d220733cee4394d54d91
Sapronov Konstantin <ks.costas@gmail.com>
2023-09-03 15:52:15 +0300

    feat: add info

--- src/app/app.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="container mx-auto mt-5">
-      <h1 className="text-center text-5xl font-extrabold dark:text-white">Hello React</h1>
-    </div>
-  );
-}
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './main.css';
-import App from '@/app/app.tsx';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = createRoot(document.getElementById('root')!);
+
+const tick = () => {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+
+  root.render(element);
+};
+
+setInterval(tick, 1000);
