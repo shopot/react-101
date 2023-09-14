14ecd0c6c56cc7dafaacfacc391d9b914bbc5a5b
Sapronov Konstantin <ks.costas@gmail.com>
2023-09-14 14:35:49 +0300

    feat: change declaration

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,11 @@
-function App() {
+import { ReactElement } from 'react';
+
+const App = (): ReactElement => {
   return (
     <div className="container mx-auto mt-5">
-      <h1 className="text-center text-5xl font-extrabold dark:text-white">Hello React</h1>
+      <h1 className="text-center text-3xl font-extrabold dark:text-white">Hello React</h1>
     </div>
   );
-}
+};
 
 export default App;
