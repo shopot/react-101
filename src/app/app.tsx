894abe70b0c5a879eca2decdda8ce0f81659e0d5
Sapronov Konstantin <ks.costas@gmail.com>
2023-09-16 09:52:38 +0300

    refactor: remove deepCopy and index state

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,7 +4,6 @@ import styles from './app.module.css';
 
 import { TodoList } from '@/components/todo-list';
 import { AddTodoForm } from '@/components/add-todo-form';
-import { deepCopy } from '@/util';
 import { Todo } from '@/types';
 
 // Начальное значение для переменной состояния todos
@@ -17,14 +16,18 @@ const initialState = [
 ];
 
 const App = (): ReactElement => {
-  // Объявляем переменную состояния для хранения списка todo при помощи useState
+  // Объявляем переменную состояния для хранения списка todo при помощи useState()
   // начальное значение - массив с одним элементом
   const [todos, setTodos] = useState<Todo[]>(initialState);
 
-  // Объявляем переменную состояния для хранения текущего значения id
-  // начальное значение = 1
-  // можно было использовать библиотеку uuid: https://www.npmjs.com/package/uuid
-  const [todoIndex, setTodoIndex] = useState(1);
+  // Функция для получения последнего id списка
+  const getLastId = () => {
+    if (todos.length === 0) {
+      return 0;
+    }
+
+    return todos[todos.length - 1].id;
+  };
 
   // Метод для переключения статуса completed
   const toggleTodoComplete = (todoId: number) => {
@@ -34,8 +37,8 @@ const App = (): ReactElement => {
     if (findIndex !== -1) {
       // Создаем полную копию списка todo, объекты нельзя скопировать простым присваиванием
       // Полная копия нужна, потому что в setTodos нужно передать новое значение,
-      // поэтому мы не можем поменять часть переменной состояния при использовании useState
-      const newState = deepCopy<Todo[]>(todos);
+      // поэтому мы не можем поменять часть переменной состояния при использовании useState()
+      const newState = structuredClone<Todo[]>(todos);
 
       const todo = newState[findIndex];
 
@@ -50,7 +53,7 @@ const App = (): ReactElement => {
   // Метод для удаления todo по id
   const removeTodo = (todoId: number) => {
     // Удаляем элемент из массива используя копию массива
-    const newState = deepCopy<Todo[]>(todos).filter(({ id }) => id !== todoId);
+    const newState = structuredClone<Todo[]>(todos).filter(({ id }) => id !== todoId);
 
     // Обновляем переменную состояния новым значением из списка todo
     setTodos(newState);
@@ -60,16 +63,13 @@ const App = (): ReactElement => {
   const addTodo = (title: string) => {
     // Создаем новый элемент для списка todo
     const newTodo = {
-      id: todoIndex + 1,
+      id: getLastId() + 1,
       title,
       completed: false,
     };
 
-    // Обновляем переменную состояния todoIndex увеличивая на 1
-    setTodoIndex(todoIndex + 1);
-
     // Получаем копию массива списка todo
-    const newState = deepCopy<Todo[]>(todos);
+    const newState = structuredClone<Todo[]>(todos);
 
     // Добавляем в него новый элемент
     newState.push(newTodo);
