513623e08de5c23f5c4d496013976d61b654787f
Sapronov Konstantin <ks.costas@gmail.com>
2023-09-22 22:21:40 +0300

    fix: change h1

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -3,7 +3,7 @@ import { ReactElement } from 'react';
 const App = (): ReactElement => {
   return (
     <div className="container mx-auto mt-5">
-      <h1 className="text-center text-3xl font-extrabold dark:text-white">Hello React</h1>
+      <h1 className="text-center text-3xl font-bold">Hello React</h1>
     </div>
   );
 };
