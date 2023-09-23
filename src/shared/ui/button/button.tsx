c3faa7f6d21ce4a6a208b718c5ddcbdb6cb9e360
Sapronov Konstantin <ks.costas@gmail.com>
2023-09-23 10:40:07 +0300

    fix: remove unnecessary code

--- src/shared/ui/button/button.tsx
+++ src/shared/ui/button/button.tsx
@@ -3,11 +3,15 @@ import { ButtonHTMLAttributes, ReactElement } from 'react';
 import styles from './button.module.css';
 
 export const Button = (props: ButtonProps): ReactElement => {
-  const { variant, type, className, ...rest } = props;
+  const { variant, type, ...rest } = props;
 
-  const computedClassName = `${styles.button} ${styles[variant || 'default']} ${className || ''}`;
-
-  return <button type={type || 'button'} className={computedClassName} {...rest} />;
+  return (
+    <button
+      type={type || 'button'}
+      className={`${styles.button} ${styles[variant || 'default']}`}
+      {...rest}
+    />
+  );
 };
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
