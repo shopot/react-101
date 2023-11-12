fa409ba8963cb10499ca28f5c37bc455cefb499e
Sapronov Konstantin <ks.costas@gmail.com>
2023-11-12 10:55:12 +0300

    fix: change import

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
-import Axios from 'axios';
+import * as Axios from 'axios';
 
 import { API_URL } from '@/config';
 
-export const axios = Axios.create({
+export const axios = Axios.default.create({
   baseURL: API_URL,
 });
