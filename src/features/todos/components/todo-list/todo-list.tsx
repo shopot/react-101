b93793b0d184b717651a135b474afb75298bdb60
Sapronov Konstantin <ks.costas@gmail.com>
2023-11-30 15:59:10 +0300

    fix: remove error code

--- src/features/todos/components/todo-list/todo-list.tsx
+++ src/features/todos/components/todo-list/todo-list.tsx
@@ -1,29 +1,13 @@
-import { JSX, useState } from 'react';
+import { JSX } from 'react';
 
 import styles from './todo-list.module.css';
 
-import { TodoItem } from '../todo-item/todo-item';
+// import { TodoItem } from '../todo-item/todo-item';
 
 export const TodoList = (): JSX.Element => {
-  const [page, setPage] = useState(1);
-
-  console.log(totalCount);
-
-  if (isError) {
-    return <div>An error has occurred!</div>;
-  }
-
-  if (isLoading || isFetching) {
-    return <div className={styles.loader}>Loading...</div>;
-  }
-
-  const todoList = results.length
-    ? results.map((todo) => <TodoItem key={todo.id} todo={todo} />)
-    : 'No data!';
-
   return (
     <>
-      <div className={styles.todoList}>{todoList}</div>
+      <div className={styles.todoList}>TodoList</div>
     </>
   );
 };
