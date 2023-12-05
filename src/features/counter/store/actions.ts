bfa65d7db690b67ffb2c4aac4ecdcf7ccc063ebb
Sapronov Konstantin <ks.costas@gmail.com>
2023-12-05 07:11:15 +0300

    refactor: format code

--- src/features/counter/store/actions.ts
+++ src/features/counter/store/actions.ts
@@ -4,6 +4,18 @@ import { AnyAction } from 'redux';
 import { DECREMENT, INCREMENT, RESET } from './constants';
 import { CounterState } from './reducer';
 
+interface Increment {
+  type: typeof INCREMENT;
+}
+
+interface Decrement {
+  type: typeof DECREMENT;
+}
+
+interface Reset {
+  type: typeof RESET;
+}
+
 // Required for reducer
 export type CounterAction = Increment | Decrement | Reset;
 
@@ -25,15 +37,3 @@ export const reset = () => {
     }, 500);
   };
 };
-
-interface Increment {
-  type: typeof INCREMENT;
-}
-
-interface Decrement {
-  type: typeof DECREMENT;
-}
-
-interface Reset {
-  type: typeof RESET;
-}
