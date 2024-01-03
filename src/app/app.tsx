bcfacdd08c4c232ec52c34a06024e7fd4fb450eb
Sapronov Konstantin <ks.costas@gmail.com>
2024-01-03 13:42:15 +0300

    fix: update import with type

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,6 @@
-import { ReactElement } from 'react';
+import { JSX } from 'react';
 
-const App = (): ReactElement => {
+const App = (): JSX.Element => {
   return (
     <div className="container mx-auto mt-5">
       <h1 className="text-center text-3xl font-bold">Hello React</h1>
