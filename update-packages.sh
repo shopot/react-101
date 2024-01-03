20862ff136f4dec0f50a359c057a5e64fb91199b
Sapronov Konstantin <ks.costas@gmail.com>
2024-01-03 15:30:57 +0300

    feat: update packages

---
+++ update-packages.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+TS_VERSION="5.2.2"
+
+update_packages() {
+  arg1=$1
+
+  PACKAGES=$(cat package.json | tr -d "\n" | tr -d " " | grep -o "\"${arg1}\":{[^}]*}" | grep -o -E '"[@a-z0-9\/\-]+"' | tr -d "\n" | sed -s "s/\"${arg1}\"//g" | sed -s 's/""/ /g' | tr -d '"' | awk '{for (i=1; i<=NF; i++) printf $i "@latest" (i < NF ? " " : "\n")}')
+
+  if [ "$arg1" = "dependencies" ]; then
+    echo "Update dependencies: ${PACKAGES}"
+
+    echo "npm install --save ${PACKAGES}" | sh
+  else
+    echo "Update devDependencies: ${PACKAGES}"
+
+    PACKAGES=`echo $PACKAGES | sed -s "s/ typescript@latest/ typescript@${TS_VERSION}/g"`;
+
+    echo "npm install --save-dev ${PACKAGES}" | sh
+  fi
+}
+
+update_packages dependencies
+
+update_packages devDependencies
