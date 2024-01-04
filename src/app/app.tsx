9379cac4e0e72474b4a4ae8281a826b1cda5f5e4
Sapronov Konstantin <ks.costas@gmail.com>
2024-01-04 17:57:15 +0300

    feat: update packages and configs

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,11 @@
-function App() {
+import { JSX } from 'react';
+
+const App = (): JSX.Element => {
   return (
     <div className="container mx-auto mt-5">
       <h1 className="text-center text-5xl font-extrabold dark:text-white">Hello React</h1>
     </div>
   );
-}
+};
 
 export default App;
